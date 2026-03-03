5a589f4a2559fbd966dd6763d72f5606da323ccb
Ali Raza <pasharaza017@gmail.com>
2026-03-03 17:25:24 +0500

    deleted all files

--- WEB LAB 33/WEB LAB 33/index.js
+++
@@ -1,9 +0,0 @@
-//index.js
-const mongoose= require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/labDB')
-.then(()=>{
-    console.log('Connected to MongoDB successfully');
-})
-.catch((err)=>{
-    console.error('Error while connecting to MongoDB',err);
-});
